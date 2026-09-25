fd03437ae9fc6d7266fae0fac2dd7b48003a9636
Arslan Wali <Arsalwali7@gmail.com>
2026-09-25 12:11:37 +0500

    baisc of python

---
+++ Phase 0 — Python & Programming/Lesson 1 Variables & Data Types/1. variable.py
@@ -0,0 +1,7 @@
+name = "Arlsan wali"
+age = 25
+is_learning = True
+height = 5.9
+
+print(type(age))
+print(type(is_learning))
---
+++ Phase 0 — Python & Programming/Lesson 1 Variables & Data Types/2. string concatenation & string interpolation.py
@@ -0,0 +1,6 @@
+age = 95
+
+
+print ("You are" +str(age)+"years old")
+
+print(f"your are {age} years old")
