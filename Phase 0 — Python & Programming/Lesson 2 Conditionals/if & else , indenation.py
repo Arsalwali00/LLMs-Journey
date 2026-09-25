5095047900088c33a2ff6a45940a8d8c05c87830
Arslan Wali <Arsalwali7@gmail.com>
2026-09-25 12:43:46 +0500

    COndtions

---
+++ Phase 0 — Python & Programming/Lesson 2 Conditionals/if & else , indenation.py
@@ -0,0 +1,27 @@
+"""sets a variable temperature = 75
+Prints "It's hot" if temperature is above 85
+Prints "It's nice out" if temperature is between 60 and 85 (inclusive)
+Prints "It's cold" otherwise"""
+
+
+temperature=75
+
+if temperature >85:
+    print("Its hot")
+elif temperature >=60:
+    print("its nice")
+else:
+    print("Its cold")
+    
+
+
+
+age = 20
+
+if age>=18:
+    print("You are adult")
+elif age >=13:
+    print("you are teenage")
+else:
+    print("you are child")
+    
